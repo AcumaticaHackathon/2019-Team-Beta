45e7a8d7851b09263adacd4b7ecc008d0eeb3dac
Brendan Hennelly <bhennelly@jaas.net>
2019-01-25 09:34:58 -0500

    Create Solution/Project

---
+++ PX.Objects.MobiPunch/Descriptor/Messages.cs
@@ -0,0 +1,37 @@
+﻿namespace PX.Objects.MobiPunch.Descriptor
+{
+//    [PXLocalizable(Messages.Prefix)]
+    public static class Messages
+    {
+        public const string Prefix = "MP";
+
+        /// <summary>
+        /// Localize the message
+        /// </summary>
+        /// <param name="msg">Message to localize</param>
+        /// <returns>Localized message</returns>
+        public static string GetLocal(string msg)
+        {
+            return GetLocal(msg, typeof(Messages));
+        }
+
+        /// <summary>
+        /// Localize the message with provided format arguments
+        /// </summary>
+        /// <param name="msg">Message to localize</param>
+        /// <param name="formatArgs">Arguments for string.Format</param>
+        /// <returns>Localized message</returns>
+        public static string GetLocal(string msg, params object[] formatArgs)
+        {
+            return string.Format(GetLocal(msg, typeof(Messages)), formatArgs);
+        }
+
+        /// <summary>
+        /// Localize the message
+        /// </summary>
+        public static string GetLocal(string msg, Type msgType)
+        {
+            return PXLocalizer.Localize(msg, msgType.ToString());
+        }
+    }
+}
