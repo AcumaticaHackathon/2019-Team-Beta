34e0193ddb616eba4b12a680f5156cbd83472688
Brendan Hennelly <bhennelly@jaas.net>
2019-01-26 18:57:08 -0600

    New page for Punch Review; Post build to copy MP page to CustomizationProject

---
+++ PX.Objects.MobiPunch/PunchReview.cs
@@ -0,0 +1,17 @@
+﻿using PX.Data;
+
+namespace PX.Objects.MobiPunch
+{
+    public class PunchReview : PXGraph<PunchReview>
+    {
+        public PXCancel<PunchEmployee> Cancel;
+
+        public PXFilter<PunchEmployee> Filter;
+
+        [PXFilterable]
+        public PXSelect<
+            PunchEmployeeActivity, 
+            Where<PunchEmployeeActivity.employeeID, Equal<Current<PunchEmployee.employeeID>>,
+                Or<Current<PunchEmployee.employeeID>, IsNull>>> EmployeeActivity;
+    }
+}
